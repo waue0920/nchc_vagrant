145f702a5fd06eb4fc5e4b462285553c88292e88
waue0920 <waue0920@gmail.com>
2016-05-10 17:05:17 +0800

    fix

--- util_tool/equip_sudo_users.sh
+++ util_tool/equip_sudo_users.sh
@@ -5,7 +5,7 @@
 
 
 while [[ -n $1 ]]; do
-    useradd -d /home/$1  -s /bin/bash -m -p $(echo $2 | openssl passwd -1 -stdin) $1
+    useradd -d /home/$1  -s /bin/bash -m -g root -p $(echo $2 | openssl passwd -1 -stdin) $1
     grep -q  "^$1" /etc/sudoers || echo "$1    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
     shift # shift all parameters;
     shift # shift all parameters;
